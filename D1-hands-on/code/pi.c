f918b94adcfc1a074cbfd69c443eeaa445ca02f9
alberto sartori <alberto.sartori86@gmail.com>
2016-08-31 17:29:54 +0200

    indented

--- D1-hands-on/code/pi.c
+++ D1-hands-on/code/pi.c
@@ -17,7 +17,7 @@ int main (int argc, char ** argv) {
 
   if (argc<2)
     {
-      printf(" Usage: %s (number) \n",argv[0]);
+      printf(" Usage: %s number \n",argv[0]);
       return 1;
     }
   int N = atoi(argv[1]);
