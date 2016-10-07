0f2f143e726f16e9d0f90d615989f39a3fac48d5
denocris <denobili16@gmail.com>
2016-10-07 18:38:12 +0200

    ultima mod

--- D4-hands-on/script_nodo.sh
+++ D4-hands-on/script_nodo.sh
@@ -11,5 +11,8 @@ module load openmpi/1.8.8/gnu/4.8.3
 
 cd $PBS_O_WORKDIR
 
-./script_da_eseguire.sh
+./script_da_eseguire.sh 6
+./script_da_eseguire.sh 11
+./script_da_eseguire.sh 16
+./script_da_eseguire.sh 21
 #./script_vsizefix.sh
