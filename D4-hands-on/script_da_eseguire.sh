52a3d020d6867f15cb183076163a23830e90d6f9
denocris <denobili16@gmail.com>
2016-10-07 18:18:25 +0200

    ultimi script nuovi3

--- D4-hands-on/script_da_eseguire.sh
+++ D4-hands-on/script_da_eseguire.sh
@@ -3,7 +3,7 @@
 #declare -a vsize=$(seq 1000 5000 21000)
 
 
-num_thrs=20
+num_thrs=1
 
 rm -f h3c_T$num_thrs.txt
 touch h3c_T$num_thrs.txt
@@ -15,7 +15,7 @@ export OMP_NUM_THREADS=$num_thrs
 
 for vect_size in $(seq 10000 50000 4000000); do
 
-    gcc -DSTREAM_ARRAY_SIZE=$vect_size -O3 -openmp stream.c -o stream.x
+    gcc -DSTREAM_ARRAY_SIZE=$vect_size -O3 -fopenmp stream.c -o stream.x
 
     ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$vect_size'/' >> h3c_T$num_thrs.txt
 
