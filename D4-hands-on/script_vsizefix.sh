7c57db816c3c68e4f88422fb2b75c03c18b733de
denocris <denobili16@gmail.com>
2016-10-07 18:54:28 +0200

    vsize fixedee

--- D4-hands-on/script_vsizefix.sh
+++ D4-hands-on/script_vsizefix.sh
@@ -3,18 +3,18 @@
 #declare -a vsize=$(seq 1000 5000 21000)
 
 
-rm -f h3c_v100K.txt
-touch h3c_v100K.txt
+rm -f h3c_v1M.txt
+touch h3c_v1M.txt
 
 #mkdir -p openmp
 
 
 for num_threads in $(seq 1 1 24); do
 
-    gcc -DSTREAM_ARRAY_SIZE=100000 -O3 -fopenmp stream.c -o stream.x
+    gcc -DSTREAM_ARRAY_SIZE=1000000 -O3 -fopenmp stream.c -o stream.x
 
     export OMP_NUM_THREADS=$num_threads
 
-    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$num_threads'/' >> h3c_v100K.txt.txt
+    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$num_threads'/' >> h3c_v1M.txt.txt
 
 done
