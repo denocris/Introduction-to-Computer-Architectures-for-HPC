f37112fd8ed76d3cee6afc83cfa41144b7e3e224
denocris <denobili16@gmail.com>
2016-10-07 17:39:24 +0200

    rottura2

--- D4-hands-on/script_da_eseguire.sh
+++ D4-hands-on/script_da_eseguire.sh
@@ -3,19 +3,18 @@
 #declare -a vsize=$(seq 1000 5000 21000)
 
 
-rm -f output_openmp10Thr.txt
-touch output_openmp10Thr.txt
+rm -f output_openmp16Th.txt
+touch output_openmp16Th.txt
 
 #mkdir -p openmp
 
 
+export OMP_NUM_THREADS=16
 
 for vect_size in $(seq 10000 50000 6000000); do
 
     gcc -DSTREAM_ARRAY_SIZE=$vect_size -O3 -openmp stream.c -o stream.x
 
-    export OMP_NUM_THREADS=24
-
-    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$vect_size'/' >> output_openmp10Thr.txt
+    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$vect_size'/' >> output_openmp16Th.txt
 
 done
