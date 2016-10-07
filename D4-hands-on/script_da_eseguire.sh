7e5f11faedc3efce1207a4c95c01aa4498e155a1
denocris <denobili16@gmail.com>
2016-10-07 18:05:55 +0200

    ultimi script nuovi

--- D4-hands-on/script_da_eseguire.sh
+++ D4-hands-on/script_da_eseguire.sh
@@ -3,18 +3,20 @@
 #declare -a vsize=$(seq 1000 5000 21000)
 
 
-rm -f output_openmp16Th.txt
-touch output_openmp16Th.txt
+num_thrs=1
+
+rm -f h3c_T$num_thrs.txt
+touch h3c_T$num_thrs.txt
 
 #mkdir -p openmp
 
 
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=$num_thrs
 
-for vect_size in $(seq 10000 50000 6000000); do
+for vect_size in $(seq 10000 50000 4000000); do
 
     gcc -DSTREAM_ARRAY_SIZE=$vect_size -O3 -openmp stream.c -o stream.x
 
-    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$vect_size'/' >> output_openmp16Th.txt
+    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$vect_size'/' >> h3c_T$num_thrs.txt
 
 done
--- D4-hands-on/script_nodo.sh
+++ D4-hands-on/script_nodo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #PBS -l nodes=1:ppn=24
-#PBS -l walltime=01:00:00
+#PBS -l walltime=00:30:00
 
 
 #(module loaded in ./bashrc)
 
 module load openmpi/1.8.8/gnu/4.8.3
 
-#cd P1.2_seed/D4-hands-on
 
 cd $PBS_O_WORKDIR
 
