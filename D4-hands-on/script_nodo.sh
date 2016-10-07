244ac815d2f9196e2ad3a75fcb978bafc4a5ab36
denocris <denobili16@gmail.com>
2016-10-07 10:42:22 +0200

    aggiunto lo script per vsize fixed

--- D4-hands-on/script_nodo.sh
+++ D4-hands-on/script_nodo.sh
@@ -4,10 +4,11 @@
 #PBS -l walltime=01:00:00
 
 
-#module loaded in ./bashrc
+#(module loaded in ./bashrc)
 
 #cd P1.2_seed/D4-hands-on
 
 cd $PBS_O_WORKDIR
 
-./script_da_eseguire.sh
+#./script_da_eseguire.sh
+./script_vsizefix.sh
