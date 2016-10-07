bda4b1f70efaedc161e9f5d8e25624fade0739ec
denocris <denobili16@gmail.com>
2016-10-07 11:15:25 +0200

    100K case

--- D4-hands-on/script_vsizefix.sh
+++ D4-hands-on/script_vsizefix.sh
@@ -3,8 +3,8 @@
 #declare -a vsize=$(seq 1000 5000 21000)
 
 
-rm -f output_vsizefix1M.txt
-touch output_vsizefix1M.txt
+rm -f output_vsizefix100K.txt
+touch output_vsizefix100K.txt
 
 mkdir -p openmp
 
@@ -13,9 +13,9 @@ for num_threads in $(seq 1 1 24); do
 
     export OMP_NUM_THREADS=$num_threads
 
-    gcc -DSTREAM_ARRAY_SIZE=1000000 -O3 -openmp stream.c -o stream.x
+    gcc -DSTREAM_ARRAY_SIZE=100000 -O3 -openmp stream.c -o stream.x
 
 
-    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$num_threads'/' >> output_vsizefix1M.txt
+    ./stream.x | grep  Triad | cut -f 2 -d: | sed 's/$/    '$num_threads'/' >> output_vsizefix100K.txt
 
 done
