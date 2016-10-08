4d6ea2c63e08089c436a793f5753509a6dfa396a
denocris <denobili16@gmail.com>
2016-10-08 16:15:50 +0200

    last c

--- D4-hands-on/script_nodo.sh
+++ D4-hands-on/script_nodo.sh
@@ -15,9 +15,14 @@ cd $PBS_O_WORKDIR
 #./script_da_eseguire.sh 11
 #./script_da_eseguire.sh 16
 #./script_da_eseguire.sh 21
-./script_vsizefix.sh 11
+./script_vsizefix.sh 1
+./script_vsizefix.sh 3
+./script_vsizefix.sh 5
+./script_vsizefix.sh 7
+./script_vsizefix.sh 10
 ./script_vsizefix.sh 13
-./script_vsizefix.sh 14
 ./script_vsizefix.sh 15
-./script_vsizefix.sh 16
 ./script_vsizefix.sh 17
+./script_vsizefix.sh 20
+./script_vsizefix.sh 25
+./script_vsizefix.sh 30
